0892134e7d36b9d8fe820e856a50328e6b5113ce
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2024-10-26 20:50:27 +0200

    Add manifest.scm

---
+++ manifest.scm
@@ -0,0 +1,13 @@
+;; What follows is a "manifest" equivalent to the command line you gave.
+;; You can store it in a file that you may then pass to any 'guix' command
+;; that accepts a '--manifest' (or '-m') option.
+
+(specifications->manifest
+  (list "r"
+        "r-dplyr"
+        "r-ggplot2"
+        "r-import"
+        "r-magrittr"
+        "r-purrr"
+        "r-readr"
+        "r-tidyr"))
